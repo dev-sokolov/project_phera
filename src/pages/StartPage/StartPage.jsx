b710aa627b660d7d19206280e139ef4ee8a485ed
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-12-12 17:48:16 +0100

    fix

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -37,7 +37,7 @@ const StartPage = () => {
     return (
         <>
             <div className={styles.content}>
-                <div className={styles.scrollFix}>
+                <div className={styles.scrollFix}></div>
                     <div className={styles.wrapLogo}>
                         <div className={styles.logo}>
                             <Logo />
@@ -58,7 +58,7 @@ const StartPage = () => {
                             </div>
                         </div>
                     </Container>
-                </div>
+                
 
             </div>
         </>
